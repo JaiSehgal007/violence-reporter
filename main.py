b186d06579c4f773455c13a62e102782e2a777a8
JaiSehgal007 <jaisehgal511@gmail.com>
2024-03-30 18:52:10 +0530

    data ingestion pipeline integrated

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
 from violenceReporter import logger
+from violenceReporter.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+
+
+
+STAGE_NAME = "Data Ingestion"
+try:
+    logger.info(f">>>>>>> stage {STAGE_NAME} started <<<<<<<")
+    obj = DataIngestionTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>>>> stage {STAGE_NAME} completed <<<<<<<\n\nX================================X")
+except Exception as e:
+    logger.exception(e)
+    raise e
